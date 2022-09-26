416cea8d315eadf4f3e4848f21cc97404cc24106
asha <ashajanebrown@gmail.com>
2022-09-26 11:53:15 +0300

    refactored AuthenticationServiceImpl

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -1,6 +1,7 @@
 package taxi.service;
 
 import java.util.Optional;
+
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
 import taxi.lib.Service;
@@ -14,10 +15,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
         Optional<Driver> driver = driverService.findByLogin(login);
-        if (driver.isPresent()) {
-            if (driver.get().getPassword().equals(password)) {
-                return driver.get();
-            }
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
+            return driver.get();
         }
         throw new AuthenticationException("Login or password is incorrect");
     }
